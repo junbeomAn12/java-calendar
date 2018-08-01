c9a7d7df61cf6a30be3219d7d579233e1ca13832
Junbeom_An <wnsqja8744@gmail.com>
2018-08-01 15:38:18 +0900

    update printf expression

--- src/calendar/Calendar.java
+++ src/calendar/Calendar.java
@@ -3,12 +3,12 @@ package calendar;
 import java.util.Scanner;
 
 public class Calendar {
-	private static final int[] MAX_DAYS = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-	
+	private static final int[] MAX_DAYS = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
 	public int getMaxDaysOfMonth(int month) {
 		return MAX_DAYS[month - 1];
 	}
-	
+
 	public void printSampleCalendar() {
 		System.out.println("일  월  화  수  목  금  토");
 		System.out.println("----------------");
@@ -17,13 +17,13 @@ public class Calendar {
 		System.out.println("15 16 17 18 19 20 21");
 		System.out.println("22 23 24 25 26 27 28");
 	}
-	
-	public static void main(String[] args) {		
+
+	public static void main(String[] args) {
 		System.out.println("월을 입력하세요 : ");
 		Calendar cal = new Calendar();
 		Scanner scanner = new Scanner(System.in);
 		int month = scanner.nextInt();
-		
+
 		System.out.println(month + "월은 " + cal.getMaxDaysOfMonth(month) + "일까지 있습니다.");
 		System.out.printf("%d월은 %d일까지 있습니다.", month, cal.getMaxDaysOfMonth(month));
 		scanner.close();
