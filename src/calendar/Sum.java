0aff1b9f6db0c1a1193e67ed5bb7a8930d11ef17
Junbeom_An <wnsqja8744@gmail.com>
2018-08-01 14:20:59 +0900

    sum

---
+++ src/calendar/Sum.java
@@ -0,0 +1,15 @@
+package calendar;
+
+import java.util.Scanner;
+
+public class Sum {
+	public static void main (String[] arguments) {
+		Scanner scanner = new Scanner(System.in);
+		String inputValue = scanner.nextLine();
+		String[] strNum = inputValue.split(" ");
+		int first = Integer.parseInt(strNum[0]);
+		int second = Integer.parseInt(strNum[1]);
+		System.out.println("두수의 합은 "+ (first + second) + "입니다.");
+		
+	}
+}
